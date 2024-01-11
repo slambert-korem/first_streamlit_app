4572a54d456a19a36e593c3c11b2fb3babb10edf
slambert-korem <107148963+slambert-korem@users.noreply.github.com>
2024-01-11 15:43:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 
 streamlit.title('My Parent New Healthly Diner')
 
-
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
